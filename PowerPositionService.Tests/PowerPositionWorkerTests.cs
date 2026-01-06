5245222224e691b5d565c2e94f157ff010cad578
Michael Samy <michael.samy@composedhealth.com>
2026-01-06 23:22:27 +0000

    Generate unit tests, add PowerService.dll to the solution

---
+++ PowerPositionService.Tests/PowerPositionWorkerTests.cs
@@ -0,0 +1,118 @@
+﻿using Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace PowerPositionService.Tests
+{
+    public class PowerPositionWorkerTests
+    {
+        private ILogger<PowerPositionWorker> _logger;
+        private Mock<IPowerService> _powerServiceMock;
+        private IOptions<PowerPositionSettings> _settings;
+        private string _testOutputDir;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = NullLogger<PowerPositionWorker>.Instance;
+            _powerServiceMock = new Mock<IPowerService>();
+            _testOutputDir = Path.Combine(Path.GetTempPath(), $"PowerPositionTests_{Guid.NewGuid()}");
+
+            _settings = Options.Create(new PowerPositionSettings
+            {
+                CsvOutputDir = _testOutputDir,
+                IntervalMins = 1
+            });
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Directory.Exists(_testOutputDir))
+            {
+                Directory.Delete(_testOutputDir, true);
+            }
+        }
+
+        [Test]
+        public async Task GeneratePowerPosition_WithValidTrades_AggregatesVolumesCorrectly()
+        {
+            // Arrange
+            var mockTrades = CreateMockTrades();
+            _powerServiceMock.Setup(x => x.GetTradesAsync(It.IsAny<DateTime>()))
+                .ReturnsAsync(mockTrades);
+
+            var worker = new PowerPositionWorker(_logger, _settings, _powerServiceMock.Object);
+
+            // Act
+            await worker.GeneratePowerPosition();
+
+            // Assert
+            _powerServiceMock.Verify(x => x.GetTradesAsync(It.IsAny<DateTime>()), Times.Once);
+            Assert.That(Directory.Exists(_testOutputDir), Is.True);
+            var files = Directory.GetFiles(_testOutputDir, "PowerPosition_*.csv");
+            Assert.That(files.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task GeneratePowerPosition_SuccessfullyGeneratesCsv()
+        {
+            // Arrange
+            var trades = CreateMockTrades();
+
+            _powerServiceMock.Setup(x => x.GetTradesAsync(It.IsAny<DateTime>()))
+                .ReturnsAsync(trades);
+
+            var worker = new PowerPositionWorker(_logger, _settings, _powerServiceMock.Object);
+
+            // Act
+            await worker.GeneratePowerPosition();
+
+            // Assert
+            var files = Directory.GetFiles(_testOutputDir, "PowerPosition_*.csv");
+            var lines = await File.ReadAllLinesAsync(files[0]);
+
+            Assert.That(lines.Length, Is.EqualTo(25));
+            Assert.That(lines[1], Does.Contain("150"));
+            Assert.That(lines[12], Does.Contain("80"));
+        }
+
+        private List<PowerTrade> CreateMockTrades()
+        {
+            PowerTrade trade1 = PowerTrade.Create(new DateTime(2015, 04, 01), 24);
+
+            for (int i = 0; i < 24; i++)
+            {
+                trade1.Periods[i] = new PowerPeriod { Period = i + 1, Volume = 100 };
+            }
+
+            PowerTrade trade2 = PowerTrade.Create(new DateTime(2015, 04, 01), 24);
+
+            for (int i = 0; i < 11; i++)
+            {
+                trade2.Periods[i] = new PowerPeriod { Period = i + 1, Volume = 50 };
+            }
+            for (int i = 11; i < 24; i++)
+            {
+                trade2.Periods[i] = new PowerPeriod { Period = i + 1, Volume = -20 };
+            }
+
+            return new List<PowerTrade> { trade1, trade2 };
+        }
+
+        private Dictionary<DateTime, double> CreateTestReport()
+        {
+            var report = new Dictionary<DateTime, double>();
+            var startTime = DateTime.Now.Date.AddHours(23).AddDays(-1);
+
+            for (int i = 0; i < 24; i++)
+            {
+                report[startTime.AddHours(i)] = 100.0 + i;
+            }
+
+            return report;
+        }
+    }
+}
